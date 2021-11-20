fa8a189c8994fc14331ddc8e50306403edb6ffbe
dinhtai0401 <dinhtai040197@gmail.com>
2021-11-20 22:19:25 +0200

    inital springboot

---
+++ src/main/java/com/digits/nosto/NostoApplication.java
@@ -0,0 +1,13 @@
+package com.digits.nosto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NostoApplication.class, args);
+	}
+
+}
