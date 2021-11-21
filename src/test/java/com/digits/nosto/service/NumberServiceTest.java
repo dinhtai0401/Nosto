93db3c54adc164392974a1933e06ca5684461b14
dinhtai0401 <dinhtai040197@gmail.com>
2021-11-21 23:17:14 +0200

    add unit test to service

--- src/test/java/com/digits/nosto/service/NumberServiceTest.java
+++ src/test/java/com/digits/nosto/service/NumberServiceTest.java
@@ -1,7 +1,31 @@
 package com.digits.nosto.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.digits.nosto.model.NumberModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
 class NumberServiceTest {
+    private final NumberService numberService;
 
+    @Autowired
+    public NumberServiceTest(NumberService numberService) {
+        this.numberService = numberService;
+    }
+
+    @Test
+     void getBigNumber() {
+        NumberModel response = numberService.getBigNumber("13");
+        String result = response.getNumber();
+        assertEquals("31", result);
+    }
+
+    @Test
+    void notGetNumber() {
+        NumberModel response = numberService.getBigNumber("7777");
+        String result = response.getNumber();
+        assertEquals("No answer", result);
+    }
 }
